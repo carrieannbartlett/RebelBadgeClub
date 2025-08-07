ed709d5c71d963d8468e5f43681dd41f91cf2372
Carrie-Ann <carrieannbartlett@gmail.com>
2025-08-07 20:04:42 +0100

    Add hunt photos
    Added new hunt photos
    Added caption to images

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 const popup = document.getElementById("popup");
 const popupImage = document.getElementById("popupImage");
+const popupCaption = document.getElementById("popupCaption");
 
 function showPopup() {
     popup.classList.add("popupVisible");
@@ -15,9 +16,11 @@ function hidePopup() {
 document.querySelectorAll("section").forEach(section => {
     section.addEventListener("click", () => {
         const imgSrc = section.getAttribute("data-image");
+        const caption = section.getAttribute("data-caption");
         console.log(imgSrc);
         if (imgSrc) {
             popupImage.src = imgSrc;
+            popupCaption.textContent = caption || "";
             showPopup();
         }
     });
